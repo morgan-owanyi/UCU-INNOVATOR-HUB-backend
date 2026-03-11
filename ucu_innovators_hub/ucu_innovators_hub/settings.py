ebf24d5e13d28b4e12c63934e70102d0d49e8dc3
morgan owanyi <morganowanyii>
2026-03-11 11:11:21 +0300

    changes made to the migration files and database

--- ucu_innovators_hub/ucu_innovators_hub/settings.py
+++ ucu_innovators_hub/ucu_innovators_hub/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'users.User'
--- ucu_innovators_hub/users/models.py
+++ ucu_innovators_hub/users/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import Group, Permission
 
 # Create your models here.
+
+
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('supervisor', 'Supervisor'),
+        ('admin', 'Admin'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    faculty = models.CharField(max_length=100, blank=True, null=True)
+    department = models.CharField(max_length=100, blank=True, null=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_groups",
+        blank=True
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions",
+        blank=True
+    )
+    def __str__(self):
+        return self.username
